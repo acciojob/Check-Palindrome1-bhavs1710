91612ae5cdb11b7e65ca6704466e7818433840df
Bhawna Sharma <119921762+bhavs1710@users.noreply.github.com>
2023-05-26 16:09:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 // complete the given function
 
 function palindrome(str){
-  // Remove non-alphanumeric characters and convert to lowercase
-  str = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
-
-  // Check if the string is empty or has a single character
-  if (str.length <= 1) {
-    return true;
-  }
-
   // Check if the string is a palindrome
   for (let i = 0; i < Math.floor(str.length / 2); i++) {
     if (str[i] !== str[str.length - 1 - i]) {
@@ -18,8 +10,6 @@ function palindrome(str){
 
   return true;
 }
-
-// Example usage
 const inputString = "race a car";
 console.log("Is Palindrome:", isPalindrome(inputString));
 
